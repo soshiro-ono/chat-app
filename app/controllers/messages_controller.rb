2e44b58604e21d83dead58ad1025871fd4249bf2
小野　総司郎 <ssosksono@gmail.com>
2020-09-11 14:16:06 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
     @message = Message.new
     @room = Room.find(params[:room_id])
   end
-
+ 
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
@@ -12,11 +12,11 @@ class MessagesController < ApplicationController
     else
       render :index
     end
-
   end
-
+ 
+ 
   private
-
+ 
   def message_params
     params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
